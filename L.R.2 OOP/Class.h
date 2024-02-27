f8d4eef97eeddc251a4336fb36ac4e2c452281f3
Arthur <dryndin2004@mail.ru>
2024-02-27 22:45:42 +0500

    Добавлено описание класса матрица (состоит из элементов класса Vector; два конструктора; считывание матрицы, заполнение ее нулями, рандомное заполнение). Также унаследованный класс квадратная матрица. (не работает считывание матрицы, исправить).

--- L.R.2 OOP/Class.h
+++ L.R.2 OOP/Class.h
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <ctime>
 
-// Сделать класс матриц, рассматривая матрицу как набор векторов. Взаимодействие с классом представить через GUI
+// Сделать класс матриц, рассматривая матрицу как набор векторов.
 
 class Vector { // добавить метод рандомного заполнения вектора, выборочного заполнения
 private:
@@ -26,15 +26,30 @@ public:
 		}
 		printf("Vector(const Vector& other)\n");
 	}
-	void FillVector() {
-
+	void ZerosFill() {
+		for (int i = 0; i < size; i++) {
+			vector[i] = 0;
+		}
 	}
 	void RandomFill() {
 		srand(time(NULL));
 		for (int i = 0; i < size; i++) {
-			vector[i] = rand();
+			vector[i] = rand() % 100;
 		}
 		printf("RandomFill(int* & vector)\n");
+	}
+	int* GetVector() {
+		return vector;
+	}
+	void ReadVector() {
+		std::cout << "[";
+		for (int i = 0; i < size; i++) {
+			std::cout << vector[i];
+			if (i < size - 1) {
+				std::cout << ' ';
+			}
+		}
+		std::cout << "]\n";
 	}
 	~Vector() {
 		delete[] vector;
@@ -42,14 +57,66 @@ public:
 	}
 };
 
-class Matrix {
-public:
-	int width, high;
-	Matrix() : width(0), high(0){
-		printf("Matrix()\n");
-	}
-	Matrix(int w, int h) : width(w), high(h) {
-		printf("Matrix(int w, int h)\n");
-	}
 
+class Matrix {
+protected:
+	int row, column;
+	Vector** matrx;
+public:
+	//добавить конструктор копирования
+	Matrix() : row(1), column(0){
+		printf("Matrix()\n");
+		matrx = new Vector*[row];
+		ZerosFill();
+	}
+	Matrix(int r, int c) : row(r), column(c) {
+		matrx = new Vector*[row];
+		printf("Matrix(int r, int c)\n");
+		ZerosFill();
+	}
+	int GetWidth() {
+		return row;
+	}
+	int GetHigh() {
+		return column;
+	}
+	// Что-то не так
+	void ReadMatrix() {
+		for (int i = 0; i < column; i++) {
+			matrx[i]->ReadVector();
+ 		}
+	}
+	void ZerosFill() {
+		for (int i = 0; i < column; i++) {
+			matrx[i]->ZerosFill();
+		}
+	}
+	virtual void RandomFill() {
+		for (int i = 0; i < column; i++) {
+			matrx[i]->RandomFill();
+		}
+	}
+	~Matrix() {
+		printf("~Matrix()\n");
+		delete[] matrx;
+	}
 };
+
+class Square_matrix : public Matrix {
+public:
+	Square_matrix(){
+		printf("Square_matrix()\n");
+		row = 1;
+		column = 1;
+	}
+	Square_matrix(int dimension) {
+		row = column = dimension;
+		printf("Square_matrix(int dimension)\n");
+	}
+	
+	void RandomFill() {
+		for (int i=0; i < column; i++) {
+		}
+	}
+};
+
--- mainClasses/Source.cpp
+++ mainClasses/Source.cpp
@@ -1,14 +1,18 @@
+#include <iostream>
 #include "Class.h"
-
 int main() {
+	//Создание векторов
 	setlocale(LC_ALL, "rus");
 	Vector a(5);
 	a.RandomFill();
 	Vector b(a);
 	b.ReadVector();
 	Vector* c = new Vector(5);
-	c->FillVector();
+	c->ZerosFill();
 	c->ReadVector();
 	delete c;
+	//////////////////////////////////
+	Matrix as(5, 2);
+	as.ReadMatrix();
 	return 0;
 }
